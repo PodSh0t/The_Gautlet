9d00c055cc9df04b59ddee1ac2c4c7dca102b049
jjohnnytt <johnnyt12312@gmail.com>
2024-06-26 00:17:33 -0400

    moved the java coffeee i love cofeee

---
+++ JS/page2.js
@@ -0,0 +1,11 @@
+function show() {
+    document.getElementById("playerID")
+        playerID.src = "/html/players.png"
+        playerID.style = "heigt:900px;width:900px;"
+    document.getElementById("text2")
+        text2.style = "color:rgb(255, 255, 255)"
+    document.getElementById("bttn")
+        .style.display = "none";
+    document.getElementById("text1")
+        .style.display = "none";
+}
