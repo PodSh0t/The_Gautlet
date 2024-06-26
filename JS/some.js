ee60b43bed1dbc9d98951740159a8f5c44a7f48c
PodSh0t~ <nickpodias@gmail.com>
2024-06-26 14:28:51 -0400

    Reformatted Files

--- JS/some.js
+++ JS/some.js
@@ -1,27 +1,25 @@
 function nuke() {
-    document.getElementById("everything")
-        .style.display = "none";
-    document.getElementById("postNuke")
-        .innerHTML = "Nice one idiot";
-};
+  document.getElementById("everything").style.display = "none";
+  document.getElementById("postNuke").innerHTML = "Nice one idiot";
+}
 //math
 function letsMultiply() {
-    let x = document.getElementById("number").innerHTML;
-    let y = x * 2;
-    document.getElementById("number").innerHTML = y;
-};
+  let x = document.getElementById("number").innerHTML;
+  let y = x * 2;
+  document.getElementById("number").innerHTML = y;
+}
 function letsDivide() {
-    let x = document.getElementById("number").innerHTML;
-    let y = x / 2;
-    document.getElementById("number").innerHTML = y;
-};
+  let x = document.getElementById("number").innerHTML;
+  let y = x / 2;
+  document.getElementById("number").innerHTML = y;
+}
 function plusOne() {
-    let x = document.getElementById("number").innerHTML;
-    x++;
-    document.getElementById("number").innerHTML = x;
-};
+  let x = document.getElementById("number").innerHTML;
+  x++;
+  document.getElementById("number").innerHTML = x;
+}
 function minusOne() {
-    let x = document.getElementById("number").innerHTML;
-    x--;
-    document.getElementById("number").innerHTML = x;
-};
+  let x = document.getElementById("number").innerHTML;
+  x--;
+  document.getElementById("number").innerHTML = x;
+}
