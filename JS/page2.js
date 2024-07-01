624758530b80c29aae6a0dabef979a807cefde0c
jjohnnytt <johnnyt12312@gmail.com>
2024-06-30 20:52:04 -0400

    css rework

--- JS/page2.js
+++ JS/page2.js
@@ -1,12 +1,15 @@
 function show() {
     document.getElementById("playerID")
         .src = "../Resources/players.png";
+        playerID.classList.add("rotate");
         playerID.style = "heigt:900px;width:900px;";
+    document.getElementById("playerContainer")
+        .classList.add("expand");
     document.getElementById("text2")
         .innerHTML = "Whoever Move First is Gay.";
         text2.style = "color:rgb(255, 255, 255)";
-    document.getElementById("bttn")
-        .style.display = "none";
     document.getElementById("text1")
+        .style.display = "none";
+    document.getElementById("bttn")
         .style.display = "none";
 };
