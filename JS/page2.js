7cc96460214e68b96a72a3db38ca96484d2697c9
jjohnnytt <johnnyt12312@gmail.com>
2024-07-01 09:22:05 -0400

    headding to ur moms hourse

--- JS/page2.js
+++ JS/page2.js
@@ -1,15 +1,19 @@
 function show() {
-    document.getElementById("playerID")
-        .src = "../Resources/players.png";
-        playerID.classList.add("rotate");
-        playerID.style = "heigt:900px;width:900px;";
-    document.getElementById("playerContainer")
-        .classList.add("expand");
-    document.getElementById("text2")
-        .innerHTML = "Whoever Move First is Gay.";
-        text2.style = "color:rgb(255, 255, 255)";
-    document.getElementById("text1")
-        .style.display = "none";
-    document.getElementById("bttn")
-        .style.display = "none";
+  document.getElementById("text1")
+    .style.display = "none";
+  document.getElementById("bttn")
+    .style.display = "none";
+  document.getElementById("playerID")
+    .src = "../Resources/players.png";
+    playerID.classList.add("expand");
+    playerID.style = "heigt:900px;width:900px;";
+  document.getElementById("playerContainer")
+    .classList.add("rotate");
+  document.getElementById("text2")
+    .innerHTML = "Whoever Move First is Gay.";
+    text2.classList.add ("expand")
+  document.getElementById("reload")
+    .style.display = "";
+    //this might not work
+    reload.style = "z-index: 1"
 };
