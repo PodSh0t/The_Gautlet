84d1f7b2b7572d0f7373274fd96257b0975ebd59
jjohnnytt <johnnyt12312@gmail.com>
2024-07-02 19:25:58 -0400

    i hate javascript and pictures

---
+++ JS/showRoom.js
@@ -0,0 +1,24 @@
+let slideIndex = 1;
+showSlide(slideIndex);
+
+function changeSlide(n) {
+    showSlide(slideIndex += n);
+}
+function currentSlide(n) {
+    showSlide(slideIndex = n);
+}
+function showSlide(n) {
+    let i;
+    let slides = document.getElementsByClassName("slide");
+    let dots = document.getElementsByClassName("dot");
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active","");
+    }
+    slides[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className += " active";
+}
