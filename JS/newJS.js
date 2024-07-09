fe1162ff52702b6f7b7e46036efa4ce8354d8f1e
PodSh0t~ <nickpodias@gmail.com>
2024-07-08 20:53:56 -0400

    dots

--- JS/newJS.js
+++ JS/newJS.js
@@ -1,16 +1,16 @@
 function nuke() {
   document.getElementById("everything").style.display = "none";
   document.getElementById("postNuke").innerHTML = "Nice one idiot";
-    postNuke.style.display = "block";
+  postNuke.style.display = "block";
 }
 //math
 function multiply(n) {
-  let x = +(document.getElementById("number").innerHTML);
+  let x = +document.getElementById("number").innerHTML;
   let y = x * n;
   document.getElementById("number").innerHTML = y;
 }
 function addition(n) {
-  let x = +(document.getElementById("number").innerHTML);
+  let x = +document.getElementById("number").innerHTML;
   x += n;
   document.getElementById("number").innerHTML = x;
 }
