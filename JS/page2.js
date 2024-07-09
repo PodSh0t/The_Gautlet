eaf7259f739862077acdba12ba6cdeb7f289042d
PodSh0t~ <nickpodias@gmail.com>
2024-07-08 20:49:49 -0400

    prettierrc

--- JS/page2.js
+++ JS/page2.js
@@ -1,17 +1,11 @@
 function show() {
-  document.getElementById("text1")
-    .style.display = "none";
-  document.getElementById("bttn")
-    .style.display = "none";
-  document.getElementById("playerID")
-    .src = "../Resources/players.png";
-    playerID.classList.add("expand");
-    playerID.style = "heigt:900px;width:900px;";
-  document.getElementById("playerContainer")
-    .classList.add("rotate");
-  document.getElementById("text2")
-    .innerHTML = "Whoever Move First is Gay.";
-    text2.classList.add ("expand")
-  document.getElementById("reload")
-    .style.display = "";
+  document.getElementById("text1").style.display = "none";
+  document.getElementById("bttn").style.display = "none";
+  document.getElementById("playerID").src = "../Resources/players.png";
+  playerID.classList.add("expand");
+  playerID.style = "heigt:900px;width:900px;";
+  document.getElementById("playerContainer").classList.add("rotate");
+  document.getElementById("text2").innerHTML = "Whoever Move First is Gay.";
+  text2.classList.add("expand");
+  document.getElementById("reload").style.display = "";
 }
--- JS/showRoom.js
+++ JS/showRoom.js
@@ -2,23 +2,27 @@ let slideIndex = 1;
 showSlide(slideIndex);
 
 function changeSlide(n) {
-    showSlide(slideIndex += n);
+  showSlide((slideIndex += n));
 }
 function currentSlide(n) {
-    showSlide(slideIndex = n);
+  showSlide((slideIndex = n));
 }
 function showSlide(n) {
-    let i;
-    let slides = document.getElementsByClassName("slide");
-    let dots = document.getElementsByClassName("dot");
-    if (n > slides.length) {slideIndex = 1}
-    if (n < 1) {slideIndex = slides.length}
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active","");
-    }
-    slides[slideIndex-1].style.display = "block";
-    dots[slideIndex-1].className += " active";
+  let i;
+  let slides = document.getElementsByClassName("slide");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
 }
